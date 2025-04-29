39bf7f83f6b23838a84686ebe4b2268e78272dcc
Enzo Bozzani <bozzanienzo@gmail.com>
2025-04-29 16:57:49 -0300

    feat - create medievalPortal - apply properties to potion prefab
    Signed-off-by: Enzo Bozzani <bozzanienzo@gmail.com>

---
+++ project/DeathsWish/Assets/Scripts/MedievalPortal.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MedievalPortal : MonoBehaviour
+{
+    private Scene cenaAtual;
+    void Start()
+    {
+        cenaAtual = SceneManager.GetActiveScene();
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("eun");
+        if (collision.CompareTag("Player"))
+        {
+            if (cenaAtual.name == "Medieval1")
+            {
+                SceneManager.LoadScene("Medieval2");
+            }
+            else if (cenaAtual.name == "Medieval2")
+            {
+                SceneManager.LoadScene("Medieval3");
+            }
+            else if (cenaAtual.name == "Medieval3")
+            {
+                SceneManager.LoadScene("MedievalBoss");
+            }
+        }
+    }
+
+
+
+}
