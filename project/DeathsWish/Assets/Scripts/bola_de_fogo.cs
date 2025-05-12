3e0913acee136538af5971083e017211398f9321
GiovanneMontalvao <gigiomontalvao@gmail.com>
2025-05-12 17:07:19 -0300

    Att fases China

---
+++ project/DeathsWish/Assets/Scripts/bola_de_fogo.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bola_de_fogo : MonoBehaviour
+{
+    private PlayerController playerController;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerController = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        if (rb.velocity != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Colidiu com: " + collision.gameObject.name);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            playerController.TakeDamage();
+
+        }
+    }
+}
---
+++ project/DeathsWish/Assets/Scripts/demons_script.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class demons_script : MonoBehaviour
+{
+    public Transform player;
+    private int cactusLife = 1;
+    private PlayerController playerController;
+
+    // Vari�veis de tiro
+    public GameObject balaPrefab;
+    public Transform pontoDisparo;
+    private float velocidadeTiro = 5f;
+    private float intervaloTiros = 2f;
+    private float distanciaDeteccao = 10f;
+
+    private float tempoProximoTiro = 0f;
+
+    void Start()
+    {
+        playerController = player.GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        float distanciaParaPlayer = Vector2.Distance(transform.position, player.position);
+        if (distanciaParaPlayer <= distanciaDeteccao)
+        {
+            if (Time.time >= tempoProximoTiro)
+            {
+                Atirar();
+                tempoProximoTiro = Time.time + intervaloTiros;
+
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("dano");
+            playerController.TakeDamage();
+        }
+
+        if (collision.CompareTag("Bullet"))
+        {
+            Destroy(collision.gameObject);
+
+            if (cactusLife < 1)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                cactusLife -= 1;
+            }
+        }
+    }
+
+    void Atirar()
+    {
+        GameObject bala = Instantiate(balaPrefab, pontoDisparo.position, Quaternion.identity);
+        Rigidbody2D rb = bala.GetComponent<Rigidbody2D>();
+
+        Vector2 direcao = (player.position - pontoDisparo.position).normalized;
+        rb.velocity = direcao * velocidadeTiro;
+
+
+
+        Destroy(bala, 10f);
+    }
+}
